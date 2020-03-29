1591f75fee11fb1a5d3b88c2c5405b4dd33bdae1
xhavit <xhavit@qq.com>
2020-03-29 17:40:42 +0800

    feat: 处理umd打包下，root情况下找不到lodash/join的问题

--- script/webpack.config.base.js
+++ script/webpack.config.base.js
@@ -34,7 +34,20 @@ const baseConfig = {
             },
         },
         // lodash
-        /^lodash\/.+$/,
+        // 排除lodash/join、lodash/template这样的引用
+        // 这里处理了umd中，root类型的情况，否则root下的依赖会变成factory(root["lodash/join"], root["Mock"]);
+        // 这个函数把root下的情况处理为了：factory(root["lodash"]["join"], root["Mock"]);
+        function externalizeLodash(context, request, callback) {
+            console.log(context);
+            if (!/^lodash\/.+$/.test(request)) return callback();
+
+            callback(null, {
+                commonjs: request,
+                commonjs2: request,
+                amd: request,
+                root: request.split('/'),
+            });
+        },
     ],
     module: {
         rules: [
